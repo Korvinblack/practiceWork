52de9dbc90bf13b52b72a747e7ac5c00a2ac2eee
KorvinBlack <korvinblack@gmail.com>
2023-03-01 17:22:16 +0200

    start

---
+++ script.js
@@ -0,0 +1,9 @@
+const numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели","число");
+const personalMovieDB = {
+    count: a = numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+console.log(numberOfFilms[+'count']);
